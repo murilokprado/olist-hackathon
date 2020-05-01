c9a0557fec43b43feae4b3f62219452ffb762f16
Valenza <matheusvvalenza@gmail.com>
2020-05-01 15:02:01 -0300

    added application header

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-header">
+    <div class="app-header__name">
+      <strong>Loja do Marcão</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  align-items: center;
+  display: flex;
+  height: 55px;
+  justify-content: center;
+  width: 100%;
+}
+
+.app-header__name {
+  border: 2px solid white;
+  border-top: none;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 16px;
+}
+</style>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./AppHeader";
+
+export default {
+  name: "Main",
+  components: {
+    AppHeader
+  }
+};
+</script>
