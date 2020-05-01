48c690221a3a83df8434deffb275e8d8f61ba806
Murilo Prado <kruzprado@hotmail.com>
2020-05-01 20:01:00 -0300

    Add insomnia

--- backend/src/main/java/com/hackathon/olist/bot/AutopilotBotController.java
+++ backend/src/main/java/com/hackathon/olist/bot/AutopilotBotController.java
@@ -3,9 +3,7 @@ package com.hackathon.olist.bot;
 import com.hackathon.olist.bot.payload.MessageResponse;
 import com.hackathon.olist.bot.service.AutopilotBotService;
 import com.hackathon.olist.sendmessage.payload.MessageRequest;
-import com.twilio.rest.chat.v2.service.Channel;
 import com.twilio.rest.chat.v2.service.channel.Message;
-import com.twilio.rest.chat.v2.service.user.UserChannel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +16,6 @@ public class AutopilotBotController {
 
     public AutopilotBotController(AutopilotBotService autopilotBotService) {
         this.autopilotBotService = autopilotBotService;
-    }
-
-    @GetMapping("create_channel")
-    public Channel createChannel() {
-        return autopilotBotService.createChannel();
-    }
-
-    @GetMapping("user_channel")
-    public UserChannel getUser() {
-        return autopilotBotService.getUser();
     }
 
     @GetMapping
