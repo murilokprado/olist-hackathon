12daa74c9210b227ba15cd41008504edc3155066
Murilo Prado <kruzprado@hotmail.com>
2020-05-01 09:37:06 -0300

    Endpoint test

--- backend/src/main/java/com/hackathon/olist/sendmessage/SendMessageController.java
+++ backend/src/main/java/com/hackathon/olist/sendmessage/SendMessageController.java
@@ -3,10 +3,10 @@ package com.hackathon.olist.sendmessage;
 import com.hackathon.olist.sendmessage.payload.MessageRequest;
 import com.hackathon.olist.sendmessage.service.SendMessageService;
 import com.twilio.rest.api.v2010.account.Message;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("send_message")
@@ -16,6 +16,25 @@ public class SendMessageController {
 
     public SendMessageController(SendMessageService sendMessageService) {
         this.sendMessageService = sendMessageService;
+    }
+
+    @GetMapping
+    public List<Client> getObject() {
+        Client client = new Client();
+        client.setId(1);
+        client.setName("Murilo");
+        client.setPhone("47XXXXXXXX");
+
+        Client client2 = new Client();
+        client2.setId(2);
+        client2.setName("Valenza");
+        client2.setPhone("47XXXXXXX");
+
+        List<Client> clients = new ArrayList<>();
+        clients.add(client);
+        clients.add(client2);
+
+        return clients;
     }
 
     @PostMapping("sms")
